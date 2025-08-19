d8ba2452c3559319a5b1176844f62b3dd3ae3ced
lauraginer <lauginmat@gmail.com>
2025-08-19 19:26:11 +0200

    makefile hecho, no hace relink y compila bien

--- philo/src/main.c
+++ philo/src/main.c
@@ -1,15 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.h                                      :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lginer-m <lginer-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/09 18:13:27 by lginer-m          #+#    #+#             */
-/*   Updated: 2025/08/19 18:15:34 by lginer-m         ###   ########.fr       */
+/*   Created: 2025/08/19 18:05:54 by lginer-m          #+#    #+#             */
+/*   Updated: 2025/08/19 19:02:17 by lginer-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "../inc/philosopher.h"
 
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	printf("Philosophers program\n");
+	return (0);
+}
